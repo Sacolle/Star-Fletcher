08edb85394cd30ab340102245b7b9204bd9be6fe
sacolle <pedro.h.b.colle@gmail.com>
2026-03-24 15:14:42 -0300

    modificado o sistema de IO para usar mmap e alterada a main para se adequar ao novo sistema

--- src/includes/io.h
+++ src/includes/io.h
@@ -5,12 +5,35 @@
 #include <stddef.h>
 #include <stdint.h>
 
-typedef int FileDesc;
+#include "err.h"
+#include "floatingpoint.h"
 
-// get the alignment and offset requirements of the system
-// returns 0 on sucess
-int io_alignment_restrictions(FileDesc fd, uint32_t* alignment, uint32_t* offset);
+typedef int file_desc_t;
 
+typedef struct io_state io_state_t;
+
+err_t io_state_init(
+    const char* filename, const size_t page_size, 
+    const size_t max_iteration_count, const size_t volume_size
+);
+
+void io_state_write_file(
+    const size_t i, const size_t j, const size_t k, const size_t t, 
+    const size_t nx, const size_t ny, const size_t nz, const FP* block
+);
+
+err_t io_state_finish();
+
+const char* io_state_get_filename();
+
+err_t io_available_huge_page_sizes(size_t max_count, size_t* outp_respc, size_t* outp_available_sizes);
+
+
+
+
+
+
+/*
 // opens the file with the following options
 // O_WRONLY : write the file
 // O_CREAT  : create if not exists 
@@ -20,16 +43,20 @@ int io_alignment_restrictions(FileDesc fd, uint32_t* alignment, uint32_t* offset
 // O_DSYNC  : always assure the write operation finishes before continuing.
 // more in https://man7.org/linux/man-pages/man2/open.2.html
 // returns 0 on sucess
-int io_open_disk_file(FileDesc *fd, const char* path);
+int io_open_disk_file(file_desc_t *fd, const char* path);
 
 // writes the data to disk
 // if the buffer used to write is not aligned the function will error
 // returns 0 on sucess
-int io_write_file_to_disk(FileDesc fd, const void* buff, size_t count);
+int io_write_file_to_disk(file_desc_t fd, const void* buff, size_t count);
 
 // closes the file
 // returns 0 on sucess
-int io_close_disk_file(FileDesc fd);
+int io_close_disk_file(file_desc_t fd);
 
+// get the alignment and offset requirements of the system
+// returns 0 on sucess
+int io_alignment_restrictions(file_desc_t fd, uint32_t* alignment, uint32_t* offset);
+*/
 
 #endif
--- src/io.c
+++ src/io.c
@@ -5,16 +5,178 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
+#include "macros.h"
+
+struct io_state {
+    // uint32_t mem_align;
+    // uint32_t mem_offset;
+    bool initialized;
+    const char* filename;
+    size_t page_size;
+    size_t file_size;
+    file_desc_t fd;
+    FP* buff;
+};
+
+io_state_t _state = { .initialized = false };
+
+io_state_t* get_io_state(){
+    return &_state;
+}
+void delete_io_state(){
+    return;
+}
+
+bool is_io_state_initialized(){
+    return get_io_state()->initialized;
+}
+
+err_t io_state_init(
+    const char* filename, const size_t page_size, 
+    const size_t max_iteration_count, const size_t volume_size
+){
+    if(is_io_state_initialized()){
+        return ME_REINITILIZATION;
+    }
+
+
+    // 1. open a file
+    int fd = 0;
+    if((fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR)) == -1){
+        return errno;
+    }
+
+    // 2. posix_fallocate it to the size
+    int err = 0;
+    const size_t total_file_size = max_iteration_count * volume_size * sizeof(FP);
+    if((err = posix_fallocate64(fd, 0, total_file_size)) != 0){
+        return err; //is errno
+    }
+    int huge_flags = MAP_HUGETLB;
+    if (page_size > 4096) { // Se for maior que uma página comum
+        huge_flags |= (__builtin_ctzl(page_size) << MAP_HUGE_SHIFT);
+    }
+
+    // 3. mmap using the large page size
+    FP* buff = NULL;
+    if(
+        (buff = (FP*) mmap(NULL, total_file_size, 
+            PROT_READ | PROT_WRITE, MAP_SHARED | huge_flags, 
+            fd, 0)
+        ) == MAP_FAILED
+    ){
+        return errno;
+    }
+
+    *get_io_state() = (io_state_t){
+        .initialized = true,
+        .buff = buff,
+        .fd = fd,
+        .filename = filename,
+        .page_size = page_size,
+        .file_size = total_file_size
+    };
+
+    return 0;
+}
+
+void io_state_write_file(
+    const size_t i, const size_t j, const size_t k, const size_t t, 
+    const size_t nx, const size_t ny, const size_t nz, const FP* block
+){
+    io_state_t* state = get_io_state();
+    FP* buff = state->buff;
+    const size_t block_size = nx * ny * nz; // should be equal to CUBE(g_block_width)
+
+    for(size_t x = 0; x < nx; x++)
+    for(size_t y = 0; y < ny; y++)
+    for(size_t z = 0; z < nz; z++){
+        // get the absolute index for the point (x, y, z) of cube (i, j, k) in the total volue
+        // then offset it by which iteration we are writing to.
+        const size_t buff_idx = block_cube_to_volume_idx(x, y, z, i, j, k) + block_size * t;
+        buff[buff_idx] = block[cube_idx(x, y, z)];
+    }
+}
+
+err_t io_state_finish(){
+    if(!is_io_state_initialized()){
+        return 0;
+    }
+
+    io_state_t* state = get_io_state();
+    err_t err = 0;
+
+    if((err = munmap(state->buff, state->file_size)) != 0){
+        return errno;
+    }
+    if((err = close(state->fd)) != 0){
+        return errno;
+    }
+
+    delete_io_state();
+    return 0;
+}
+
+const char* io_state_get_filename(){
+    return get_io_state()->filename;
+}
+
+err_t io_available_huge_page_sizes(size_t max_count, size_t* outp_respc, size_t* outp_available_sizes){
+    struct dirent* dent;
+    DIR* srcdir;
+    *outp_respc = 0;
+
+    if ((srcdir = opendir("/sys/kernel/mm/hugepages")) == NULL){
+        return errno;
+    }
+
+    while((dent = readdir(srcdir)) != NULL){
+
+        const char* folder = dent->d_name;
+        const char* prepend = "hugepages-";
+        const size_t prepend_size = strlen(prepend);
+        if(strlen(folder) < prepend_size){
+            continue;
+        }
+        if(strncmp(folder, prepend, prepend_size) == 0){
+            char* out = "\0\0";
+
+            const size_t res = strtoull(folder + prepend_size, &out, 10);
+            if(errno){
+                return errno;
+            }
+
+            if(strncmp(out, "kB", 2) != 0){
+                return ME_INCOMPLETE_PARSE;
+            }
+            if(*outp_respc < max_count){
+                outp_available_sizes[*outp_respc]  = res * 1024;
+            }
+            *outp_respc += 1;
+        }
+    }
+    closedir(srcdir);
+    return 0;
+}
+
+
+
+/*
 #define HAS_FLAG(x, f) !(~(x) & f)
-
-int io_alignment_restrictions(FileDesc fd, uint32_t* alignment, uint32_t* offset){
+err_t io_alignment_restrictions(file_desc_t fd, uint32_t* alignment, uint32_t* offset){
     struct statx stx;
     if(statx(fd, "", AT_EMPTY_PATH, STATX_DIOALIGN, &stx) != 0){
         return errno;
     }
     if(!HAS_FLAG(stx.stx_mask, STATX_DIOALIGN)){
-        return -1;
+        return ME_MISSING_ATTR;
     }
     *alignment = stx.stx_dio_mem_align;
     *offset = stx.stx_dio_offset_align;
@@ -22,7 +184,7 @@ int io_alignment_restrictions(FileDesc fd, uint32_t* alignment, uint32_t* offset
     return 0;
 }
 
-int io_open_disk_file(FileDesc *fd, const char* path){
+int io_open_disk_file(file_desc_t *fd, const char* path){
     // not clear which permissions are necessary
     // NOTE: could use O_TMPFILE for the files of each tread and
     // add the congealing operation into the program. 
@@ -32,8 +194,7 @@ int io_open_disk_file(FileDesc *fd, const char* path){
     }
     return 0;
 }
-
-int io_write_file_to_disk(FileDesc fd, const void* buff, size_t count){
+int io_write_file_to_disk(file_desc_t fd, const void* buff, size_t count){
     ssize_t res = write(fd, buff, count);
     if(res != count){
         return -1;
@@ -44,9 +205,10 @@ int io_write_file_to_disk(FileDesc fd, const void* buff, size_t count){
     return 0;
 }
 
-int io_close_disk_file(FileDesc fd){
+int io_close_disk_file(file_desc_t fd){
     if(close(fd) == -1){
         return errno;
     }
     return 0;
 }
+*/
